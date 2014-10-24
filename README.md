53a44cc094b50b161850de933d14140b36a26788
cjgammon <cj.gammon@gmail.com>
2014-10-24 10:59:24 -0500

    add base files

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-SXSW-2015
-=========
+#Evolution of the Web
+____________
+
+Presentation on how the web is changing presented at SXSW Interactive 2015
+
+##Instructions
+
+To run you must do the following:
+
+* Ensure you have [bower](http://bower.io/) installed
+* In the command line navigate to the root of the repository and run `bower install`
+* For browser security restrictions the site must be run on a server either locally or remotely.
---
+++ src/js/app.js
@@ -0,0 +1,66 @@
+/*global define $ requestAnimationFrame*/
+
+define(function (require) {
+	
+	var App,
+		Backbone = require('backbone'), 
+		BgView = require('app/views/bg-view'),
+		NotesView = require('pres/views/notes-view'),
+		SlideBasicView = require('app/views/slide-basic-view'),
+		SequenceView = require('app/views/sequence-view'),
+		VideoView = require('app/views/video-view'),
+		IframeView = require('app/views/iframe-view'),
+		IframeFullView = require('app/views/iframe-full-view'),
+        AppBase = require('pres/views/app-base');
+
+    App = AppBase.extend({
+		BASE_VIEW: SlideBasicView,
+		
+		SLIDEVIEW_LIST: [
+			{cl: 'iframe-full', view: IframeFullView},
+			{cl: 'iframe', view: IframeView},
+			{cl: 'video', view: VideoView},
+			{cl: 'sequence', view: SequenceView}
+		],
+	
+        initialize: function () {
+            if (this.passTest() !== true) {
+				return;
+			}
+
+            AppBase.prototype.initialize.call(this);
+
+            this.notesView = new NotesView();
+
+			//this.bg = new BgView();
+			this.render();
+        },
+
+		passTest: function () {	
+			if (Modernizr.flexbox !== true) {
+				return false;
+			} else if (Modernizr.touch === true) {
+				$('video').each(function () {
+					$(this).attr({
+						'src': $(this).data('src')
+					});
+				});
+				return false;
+			} else {
+				return true;
+			}
+        },
+
+        render: function () {
+	
+            AppBase.prototype.render.call(this);
+
+            if (this.bg) {
+			    this.bg.render();
+            }
+
+        }
+    });
+
+	return new App();
+});
