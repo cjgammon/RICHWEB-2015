1315dcabe361b48a3402d8f9ed84e1fddc07d59a
cjgammon <cj.gammon@gmail.com>
2015-01-04 23:17:42 -0600

    add new demos

--- src/js/app/views/gsap-code-view.js
+++ src/js/app/views/gsap-code-view.js
@@ -1,27 +1,27 @@
 define(function (require) {
-	
+
 	var Backbone = require('backbone'),
 		Vars = require('pres/models/vars'),
 		AppEvent = require('pres/events/app-event'),
 		SlideView = require('pres/views/slide-view'),
 		SlideBasicView;
-		
+
 	require('tweenmax');
 	require('snap');
-	
+
 	SlideBasicView = SlideView.extend({
-		
+
 		initialize: function () {
 			this.s = Snap('#gsap-code-svg');
 			this.circle = this.s.select('#gsap-code-circle');
 			this.xstring = $('#gsap-code-x');
 			this.ystring = $('#gsap-code-y');
-			this.x = window.innerWidth / 2;
-			this.y = window.innerHeight / 4;
+			this.x = 1024 / 2;
+			this.y = 768 / 4;
 				
 			SlideView.prototype.initialize.call(this);
 		},
-		
+
 		resolve: function () {
 			var slides = Vars.get('slides'),
 				currentSlide = Vars.get('currentSlide'),
@@ -43,18 +43,15 @@ define(function (require) {
 			}
 		},
 
-		update: function () {
-			var eases = [Back.easeOut, Quad.easeOut, Circ.easeInOut, Quint.easeIn],
-				r = Math.floor(Math.random() * eases.length);
-			
+		update: function () {	
 			TweenMax.to(this.circle, 1, {snap: {tx: this.x, ty: this.y}});
 			this.xstring.text(this.x);
 			this.ystring.text(this.y);
 			
-			this.x = Math.round(50 + Math.random() * window.innerWidth - 100);
-			this.y = Math.round(50 + Math.random() * window.innerHeight - 100);
+			this.x = Math.round(100 + Math.random() * (1024 - 200));
+			this.y = Math.round(100 + Math.random() * (768 - 200));
 		},
-		
+
 		desolve: function () {
 			if (this.active) {
 				this.active = false;
@@ -63,6 +60,6 @@ define(function (require) {
 		}
 		
 	});
-	
+
 	return SlideBasicView;
 });
